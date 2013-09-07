9f011130fcc8b86884bcdf6b6e668c15f71f3370
Michel Kraemer <michel@spamihilator.com>
2013-09-07 19:36:25 +0200

    Added item data provider that provides items from a given list

---
+++ src/main/java/de/undercouch/citeproc/ListItemDataProvider.java
@@ -0,0 +1,43 @@
+// Copyright 2013 Michel Kraemer
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package de.undercouch.citeproc;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import de.undercouch.citeproc.csl.CSLItemData;
+
+/**
+ * Provides item data from a given list
+ * @author Michel Kraemer
+ */
+public class ListItemDataProvider implements ItemDataProvider {
+	private Map<String, CSLItemData> items = new HashMap<String, CSLItemData>();
+	
+	/**
+	 * Creates a data provider that serves items from the given list
+	 * @param items the items to serve
+	 */
+	public ListItemDataProvider(CSLItemData... items) {
+		for (CSLItemData i : items) {
+			this.items.put(i.getId(), i);
+		}
+	}
+
+	@Override
+	public CSLItemData retrieveItem(String id) {
+		return items.get(id);
+	}
+}
