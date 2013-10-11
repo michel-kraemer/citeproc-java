9da43d0665c28415d820bcb83625b30736ea8796
Michel Kraemer <michel@spamihilator.com>
2013-10-11 07:33:12 +0200

    Switched to official vagrant box

--- _vagrant/install.sh
+++ _vagrant/install.sh
@@ -2,11 +2,13 @@
 
 if [ ! -f /etc/apt/sources.list.d/brightbox-ruby-ng-experimental-raring.list ]
 then
+    aptitude update
+    aptitude -y install python-software-properties
     add-apt-repository ppa:brightbox/ruby-ng-experimental
 fi
 
 aptitude update
-aptitude -y install curl ruby2.0 ruby2.0-dev
+aptitude -y install curl ruby2.0 ruby2.0-dev make
 
 echo "export LC_ALL=en_US.UTF-8" > /etc/profile.d/default_encoding.sh
 chmod +x /etc/profile.d/default_encoding.sh
