2ed664094f81e4870107a88dbf06d5761809c066
Michel Kraemer <michel@undercouch.de>
2013-12-23 16:33:13 +0100

    Skip extra commas after BibTeX entries

--- src/main/java/de/undercouch/citeproc/bibtex/BibTeXReader.java
+++ src/main/java/de/undercouch/citeproc/bibtex/BibTeXReader.java
@@ -19,10 +19,14 @@ import java.io.PushbackReader;
 import java.io.Reader;
 
 /**
- * A special reader for BibTeX files skipping the header
+ * A special reader for BibTeX files skipping header and extra
+ * commas after entries
  * @author Michel Kraemer
  */
 public class BibTeXReader extends PushbackReader {
+	private int level = 0;
+	private boolean inescape = false;
+	
 	/**
 	 * @see PushbackReader#PushbackReader(Reader)
 	 * @throws IOException if <code>in</code> could not be read
@@ -49,5 +53,48 @@ public class BibTeXReader extends PushbackReader {
 			}
 		}
 		unread(c);
+	}
+	
+	@Override
+	public int read() throws IOException {
+		int r = super.read();
+		if (r >= 0) {
+			return filterCharacter((char)r);
+		}
+		return r;
+	}
+	
+	@Override
+	public int read(char cbuf[], int off, int len) throws IOException {
+		int r = super.read(cbuf, off, len);
+		if (r > 0) {
+			for (int i = 0; i < r; ++i) {
+				cbuf[i + off] = filterCharacter(cbuf[i + off]);
+			}
+		}
+		return r;
+	}
+	
+	private char filterCharacter(char c) {
+		if (inescape) {
+			inescape = false;
+			return c;
+		}
+		
+		inescape = (c == '\\');
+		
+		if (c == '{') {
+			++level;
+		} else if (c == '}') {
+			--level;
+		}
+		
+		//skip every extra comma on level 0. replace it by
+		//a whitespace character
+		if (level == 0 && c == ',') {
+			c = ' ';
+		}
+		
+		return c;
 	}
 }
